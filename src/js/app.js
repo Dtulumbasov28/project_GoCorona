b026c34e419ea229ec33fb1230f737ff57a417b0
Denis Tulumbasov <tulumbasov.denis00@mail.ru>
2022-08-08 18:48:16 +0300

    Верстка завершена. Осталось устранить проблему со скроллом при открытом бургер-меню

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,9 @@
 
 import "../scss/style.scss";
-
+let body = document.querySelector("body")
+let menuBtn = document.querySelector(".icon-menu")
+let menu = document.querySelector(".menu__body")
+menuBtn.addEventListener("click", function () {
+	menu.classList.toggle("active")
+	menuBtn.classList.toggle("active")
+})
