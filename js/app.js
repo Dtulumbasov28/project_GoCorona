9a419391ed98faa3955d83cab994c6c1388c3c6e
Denis Tulumbasov <tulumbasov.denis00@mail.ru>
2022-08-09 12:20:02 +0300

    Updates

---
+++ js/app.js
@@ -0,0 +1,20 @@
+(() => {
+    "use strict";
+    let body = document.querySelector("body");
+    let menuBtn = document.querySelector(".icon-menu");
+    let menu = document.querySelector(".menu__body");
+    menuBtn.addEventListener("click", (function() {
+        menu.classList.toggle("active");
+        menuBtn.classList.toggle("active");
+        body.classList.toggle("no-scroll");
+    }));
+    const smoothLinks = document.querySelectorAll("a[href*='#']");
+    for (let smoothLink of smoothLinks) smoothLink.addEventListener("click", (function(e) {
+        e.preventDefault();
+        const id = smoothLink.getAttribute("href");
+        document.querySelector(id).scrollIntoView({
+            behavior: "smooth",
+            block: "start"
+        });
+    }));
+})();
